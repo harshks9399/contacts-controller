1dba804a2c00b092c2378ee281479cc4d473db9d
Harsh Kumar Singh <harsh.singh@iitg.ac.in>
2024-08-13 00:23:39 +0530

    defining basic CRUD operations

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const {getContact,
-        getContacts,
-        addContact, 
-        updateContact, 
-        deleteContact} = require('../controllers/contactController');
+const { getContact, getContacts, addContact, updateContact, delContact } = require('../controller/contactsController');
 
 
-router.route("/").get(getContacts)
 
-router.route("/").post(addContact)
+// router.route("/").get(getContacts)
 
-router.route("/:id").get(getContact)
+// router.route("/").post(addContact)
+router.route("/").get(getContacts).post(addContact);
 
-router.route("/:id").put(updateContact)
 
-router.route("/:id").delete(deleteContact)
+// router.route("/:id").get(getContact)
+
+// router.route("/:id").put(updateContact)
+
+// router.route("/:id").delete(deleteContact)
+router.route("/:id").get(getContact).put(updateContact).delete(delContact);
 
 
 module.exports = router;
