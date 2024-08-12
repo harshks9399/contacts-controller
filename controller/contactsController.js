28b69afb15484495b164f9eeec8f88eecf0680d7
Harsh Kumar Singh <harsh.singh@iitg.ac.in>
2024-08-13 00:22:43 +0530

    Addingbasic functions

--- controller/contactsController.js
+++ controller/contactsController.js
@@ -1,23 +1,28 @@
 
 
 const getContacts = (req, res) => {
-    res.send("GET all contacts:");
+    // res.send("GET all contacts:");
+    res.status(200).jsonp({message : "GET all contacts"});
 }
 
 const getContact = (req, res) => {
-    res.send(`GET contact info for ${id}`);
+    // res.send(`GET contact info for ${id}`);
+    res.status(200).jsonp({message : `GET contact info for ${req.params.id}`});
 }
 
 const updateContact = (req, res) => {
-    res.send(`UPDATE contact for ${id}`);
+    // res.send(`UPDATE contact for ${id}`);
+    res.status(201).jsonp({message : `UPDATE contact for ${req.params.id}`});
 }
 
 const addContact = (req, res) => {
-    res.send("ADD contact");
+    // res.send("ADD contact");
+    res.status(201).jsonp({message : "ADD contact"});
 }
 
 const delContact = (req,res) => {
-    res.send(`DELETE contact for ${id}`);
+    // res.send(`DELETE contact for ${id}`);
+    res.status(201).jsonp({message : `DELETE contact for ${req.params.id}`});
 }
 
 
@@ -28,3 +33,5 @@ module.exports = {
     addContact,
     delContact
 };
+
+// console.log("working fine");
