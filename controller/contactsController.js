c385fb0840825ab316631190b18ab139c8216731
Harsh Kumar Singh <harsh.singh@iitg.ac.in>
2024-08-13 13:55:45 +0530

    made all functions async + added express-async-handler

--- controller/contactsController.js
+++ controller/contactsController.js
@@ -1,29 +1,39 @@
+const asyncHandler = require("express-async-handler")
 
 
-const getContacts = (req, res) => {
+const getContacts = asyncHandler (async (req, res) => {
     // res.send("GET all contacts:");
     res.status(200).jsonp({message : "GET all contacts"});
-}
+})
 
-const getContact = (req, res) => {
+const getContact = asyncHandler (async (req, res) => {
     // res.send(`GET contact info for ${id}`);
     res.status(200).jsonp({message : `GET contact info for ${req.params.id}`});
-}
+})
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler (async (req, res) => {
     // res.send(`UPDATE contact for ${id}`);
     res.status(201).jsonp({message : `UPDATE contact for ${req.params.id}`});
-}
+})
 
-const addContact = (req, res) => {
+const addContact = asyncHandler (async (req, res) => {
     // res.send("ADD contact");
-    res.status(201).jsonp({message : "ADD contact"});
-}
+    console.log(`The reqwuest body is ` + JSON.stringify(req.body));
 
-const delContact = (req,res) => {
+    const {name, email, phone} = req.body;
+
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields as needed");
+    }
+
+    res.status(201).jsonp({message : "ADD contact"});
+})
+
+const delContact = asyncHandler (async (req,res) => {
     // res.send(`DELETE contact for ${id}`);
     res.status(201).jsonp({message : `DELETE contact for ${req.params.id}`});
-}
+})
 
 
 module.exports = {
