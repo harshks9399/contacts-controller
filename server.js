3e20789b23abe2cfab1b7149a6dec32d5612dfa6
Harsh Kumar Singh <harsh.singh@iitg.ac.in>
2024-08-13 13:54:57 +0530

    added middleware for errorHandler

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
 
 
 const app = express();
 PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // this is used to parse the incoming request with JSON payloads. IMPORTANT
 app.use("/api/contacts", require("./routes/contactRoutes"));
-
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Listening on port number ${PORT}`);
