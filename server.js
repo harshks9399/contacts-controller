92a62ca1f98f50f1bb3b5c1d9a82580420c1fbcf
Harsh Kumar Singh <harsh.singh@iitg.ac.in>
2024-08-13 18:30:19 +0530

    Next step : Authorization

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
+const connectDB = require('./config/dbConnection');
 
 
-const app = express();
-PORT = process.env.PORT || 5000;
+connectDB();   // connecting to database
+const app = express(); // creating an instance of express
+PORT = process.env.PORT || 5000; // setting the port number
 
 app.use(express.json()); // this is used to parse the incoming request with JSON payloads. IMPORTANT
-app.use("/api/contacts", require("./routes/contactRoutes"));
-app.use(errorHandler)
+app.use("/api/contacts", require("./routes/contactRoutes"));  // this is used to route the incoming request to the contacts route
+app.use(errorHandler) // this is used to handle the error
 
 app.listen(PORT, () => {
     console.log(`Listening on port number ${PORT}`);
